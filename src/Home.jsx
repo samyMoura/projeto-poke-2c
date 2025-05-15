5a28f14f2a895f33dc8a7fdfd5130718509dd7e7
samyMoura <rm94773@estudante.fieb.edu.br>
2025-05-15 12:55:36 +0000

    flex

--- src/Home.jsx
+++ src/Home.jsx
@@ -7,7 +7,10 @@ const Home = () => {
         <div>
            <img src={poke} />
            <img src={eevee} />
-        
+            <img src={poke} />
+           <img src={eevee} />
+            <img src={poke} />
+           <img src={eevee} />
         </div>
 
     )
